1a38cb4de34ff79f1ff71c311bb161d91c1dd68b
Izzy Beraja <IzzyBeraja+github@gmail.com>
2022-08-16 21:11:53 -0700

    Removed all the red errors and added more to tsconfig

--- drank2/App.tsx
+++ drank2/App.tsx
@@ -1,12 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { useCallback } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  ListRenderItem,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
-export default function App() {const [isLoading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
+type Item = {
+  idDrink: string;
+  strDrink: string;
+};
+
+export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState<Item[]>([]);
 
   const getMovies = async () => {
-     try {
-      const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail');
+    try {
+      const response = await fetch(
+        "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
+      );
       const json = await response.json();
       setData(json.drinks);
     } catch (error) {
@@ -14,23 +30,28 @@ export default function App() {const [isLoading, setLoading] = useState(true);
     } finally {
       setLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
     getMovies();
   }, []);
 
+  const renderItem: ListRenderItem<Item> = useCallback(
+    ({ item }) => <Text>{item.strDrink}</Text>,
+    []
+  );
+
   return (
     <View style={{ flex: 1, padding: 24 }}>
-      {isLoading ? <ActivityIndicator/> : (
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
         <FlatList
           data={data}
           keyExtractor={({ idDrink }) => idDrink}
-          renderItem={({ item }) => (
-            <Text>{item.strDrink}</Text>
-          )}
+          renderItem={renderItem}
         />
       )}
     </View>
   );
-};
+}
