9fba8405a82744dcd4562920f1bc066035b1a7bb
Izzy Beraja <IzzyBeraja+github@gmail.com>
2022-08-16 21:54:18 -0700

    Created a basic drinks component with some styles

--- drank2/components/DrinkItem.tsx
+++ drank2/components/DrinkItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
 
 export type DrinkType = {
   id: string;
@@ -12,14 +12,37 @@ export type DrinkProps = {
 };
 
 export default function DrinkItem({ drink }: DrinkProps) {
-  const { id, name, imageUri } = drink;
+  const { name, imageUri } = drink;
 
   return (
-    <View>
-      <Image source={{ uri: imageUri }} style={{ width: 400, height: 400 }} />
-      <Text>
-        {id}: {name}
-      </Text>
+    <View style={styles.container}>
+      <Image source={{ uri: imageUri }} style={styles.image} />
+      <View style={styles.titleContainer}>
+        <Text style={styles.text}>{name}</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 12,
+    overflow: "hidden",
+    position: "relative",
+  },
+  image: {
+    width: 400,
+    height: 400,
+  },
+  titleContainer: {
+    position: "absolute",
+    bottom: 0,
+    padding: 20,
+    backgroundColor: "rgba(0,0,0, .5)",
+    width: "100%",
+  },
+  text: {
+    color: "white",
+    fontWeight: "800",
+  },
+});
