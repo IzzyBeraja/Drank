c8ee5e473a68dac67707fa04d1deec5cd526ce25
Izzy Beraja <IzzyBeraja+github@gmail.com>
2022-08-23 21:30:57 -0700

    Added a Link query

--- IPA/src/graphql/Link.ts
+++ IPA/src/graphql/Link.ts
@@ -1,1 +1,36 @@
-export * from "./Link";
+import { NexusGenObjects } from "./../../nexus-typegen";
+import { extendType, objectType } from "nexus";
+
+export const Link = objectType({
+  name: "Link",
+  definition(t) {
+    t.nonNull.id("id");
+    t.nonNull.string("description");
+    t.nonNull.string("url");
+  },
+});
+
+let links: NexusGenObjects["Link"][] = [
+  {
+    id: "1",
+    description: "this is the first link",
+    url: "fakeUrl.com",
+  },
+  {
+    id: "2",
+    description: "this is the second link",
+    url: "fakeUrl2.com",
+  },
+];
+
+export const LinkQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.nonNull.list.nonNull.field("feed", {
+      type: "Link",
+      resolve() {
+        return links;
+      },
+    });
+  },
+});
--- IPA/src/graphql/index.ts
+++ IPA/src/graphql/index.ts
@@ -1,10 +1,1 @@
-import { objectType } from "nexus";
-
-export const Link = objectType({
-  name: "Link",
-  definition(t) {
-    t.nonNull.id("id");
-    t.nonNull.string("description");
-    t.nonNull.string("url");
-  },
-});
+export * from "./Link";
