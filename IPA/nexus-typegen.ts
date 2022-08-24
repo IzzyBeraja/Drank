d298ea99a33101f7bcea682452cdee55e0bdee1c
Izzy Beraja <IzzyBeraja+github@gmail.com>
2022-08-23 21:17:38 -0700

    Moved to another folder and added new Link type

--- IPA/nexus-typegen.ts
+++ IPA/nexus-typegen.ts
@@ -28,6 +28,11 @@ export interface NexusGenScalars {
 }
 
 export interface NexusGenObjects {
+  Link: { // root type
+    description: string; // String!
+    id: string; // ID!
+    url: string; // String!
+  }
   Query: {};
 }
 
@@ -42,12 +47,22 @@ export type NexusGenRootTypes = NexusGenObjects
 export type NexusGenAllTypes = NexusGenRootTypes & NexusGenScalars
 
 export interface NexusGenFieldTypes {
+  Link: { // field return type
+    description: string; // String!
+    id: string; // ID!
+    url: string; // String!
+  }
   Query: { // field return type
     ok: boolean; // Boolean!
   }
 }
 
 export interface NexusGenFieldTypeNames {
+  Link: { // field return type name
+    description: 'String'
+    id: 'ID'
+    url: 'String'
+  }
   Query: { // field return type name
     ok: 'Boolean'
   }
---
+++ IPA/src/graphql/index.ts
@@ -0,0 +1,10 @@
+import { objectType } from "nexus";
+
+export const Link = objectType({
+  name: "Link",
+  definition(t) {
+    t.nonNull.id("id");
+    t.nonNull.string("description");
+    t.nonNull.string("url");
+  },
+});
